917a78f81ef9198feb8996ddeb2a03f912e3c168
Damien Grzywacz <d.m.g.1990n@gmail.com>
2013-08-01 14:14:30 +0200

    Day 6/23 reached

--- apps/frontend/modules/job/actions/actions.class.php
+++ apps/frontend/modules/job/actions/actions.class.php
@@ -12,9 +12,7 @@ class jobActions extends sfActions
 {
   public function executeIndex(sfWebRequest $request)
   {
-    $this->jobeet_jobs = Doctrine_Core::getTable('JobeetJob')
-      ->createQuery('a')
-      ->execute();
+    $this->categories = Doctrine_Core::getTable('JobeetCategory')->getWithJobs();
   }
 
   public function executeShow(sfWebRequest $request)
@@ -74,6 +72,14 @@ class jobActions extends sfActions
 
       $this->redirect('job/edit?id='.$jobeet_job->getId());
     }
+  }
+  public function getActiveJobs()
+  {
+    $q = $this->createQuery('j')
+      ->where('j.expires_at > ?', date('Y-m-d H:i:s', time()))
+      ->orderBy('j.expires_at DESC');
+ 
+    return $q->execute();
   }
 }
 ?>
